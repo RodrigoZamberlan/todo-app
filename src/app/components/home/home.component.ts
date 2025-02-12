0db29f26e920df5bf87a3ef868b2b8f0b5ee47fc
RodrigoZamberlan <rodrigozfreddo@outlook.com>
2025-02-12 16:45:53 -0300

    fix to be a simple bidirectional bind instead of using reactive approach with a service using rxjs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Task } from '../../models/Task.model';
 
 @Component({
   selector: 'app-home',
@@ -7,20 +8,25 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
-  list: string[] = ["first task"];
-  newTask: string = "";
+  tasks: Task[] = [];
+  newTask: Task["description"] = "";
   error: string = "";
 
   addNewTask() {
-    if (this.newTask.length > 0) {
-      if (!this.list.includes(this.newTask)) {
-        this.list.push(this.newTask);
-        this.newTask = "";
-        this.error = "";
-      } else {
-        this.error = "This item already exist";
-      }
+    if (this.newTask.trim().length === 0) {
+      this.error = "The task description cannot be empty";
+      return;
     }
+
+    if (this.tasks.some((task) => task.description === this.newTask)) {
+        this.error = "This task already exist on the list";
+        return;
+    }
+    
+    this.tasks.push({description: this.newTask, isDone: false});
+    this.newTask = "";
+    this.error = "";
   }
 }
